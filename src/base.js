41d1043eaffb3fa03e8444d812ea015433546cde
Luca Vazzano <luca.vazzano@gmx.net>
2019-05-05 00:34:14 +0200

    basic fetching of ScoreSaber data

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ const twitch = window.Twitch.ext
 const rlog = twitch.rig.log
 
 
-twitch.onContext(function(context, changedKeys) {
+twitch.onContext((context, changedKeys) => {
 	if (context.theme === 'dark') {
 		document.getElementById('root').classList.add('dark')
 	} else {
