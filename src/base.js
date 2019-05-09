518063701c1f4dbea26d0c0fa2fb3f6c3435cd8f
Luca Vazzano <luca.vazzano@gmx.net>
2019-05-09 20:53:54 +0200

    improve handling of onContext

--- src/base.js
+++ src/base.js
@@ -1,13 +1,24 @@
 const twitch = window.Twitch.ext
 const rlog = twitch.rig.log
 
+const state = {
+	isPlayingBeatSaber: false,
+	scoreSaberId: undefined,
+	language: 'en',
+	accentColor: '008139'
+}
+
 
 twitch.onContext((context, changedKeys) => {
-	if (context.theme === 'dark') {
-		document.getElementById('root').classList.add('dark')
-	} else {
-		document.getElementById('root').classList.remove('dark')
+	if (changedKeys.includes('theme')) {
+		if (context.theme === 'dark') {
+			document.getElementById('root').classList.add('dark')
+		} else {
+			document.getElementById('root').classList.remove('dark')
+		}
 	}
 
-	rlog(context.game)
+	if (changedKeys.includes('game')) {
+		state.isPlayingBeatSaber = (context.game === 'Beat Saber');
+	}
 })
