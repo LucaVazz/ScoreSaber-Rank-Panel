0163f4da8fdafe9a00511d42cecf1a9c987d8a29
Luca Vazzano <luca.vazzano@gmx.net>
2019-05-09 21:00:29 +0200

    add auto-refresh if playing BeatSaber

--- src/panel.js
+++ src/panel.js
@@ -9,6 +9,8 @@ const ICON_UP = '<path d="M345.6 128l51.3 51.3-109.3 109.4-89.6-89.6L32 365.4 63
 const ICON_DOWN = '<path d="M480 397V262.5l-51.3 51.3-141.1-141-89.6 89.7L63.6 128 32 159.6l166 166.3 89.6-89.7 109.3 109.4-51.3 51.4H480z"/>'
 
 function fetchData() {
+    rlog('Fetching ScoreSaber-Data...')
+
     return fetch(new Request(DATA_URL))
         .then(response => {
             if (!response.ok) {
@@ -90,7 +92,11 @@ fetchData()
         document.getElementById('content').classList.remove('hidden')
     })
     .then(() => {
-        // TODO: auto-refresh
+        window.setInterval(() => {
+            if (state.isPlayingBeatSaber) {
+                fetchData().then(() => {})
+            }
+        }, 60 * 1000)
     })
     .catch(err => {
         document.getElementById('load-splash-text').innerText = ':/'
