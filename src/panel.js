034ab060badf58407685f6cd99f3b66aa066f45d
Luca Vazzano <luca.vazzano@gmx.net>
2019-05-14 21:46:57 +0200

    move functions for ScoreSaber fetching to separate file

--- src/panel.js
+++ src/panel.js
@@ -1,58 +1,21 @@
-const DATA_URL = new URL('https://scoresaber-proxy.lucavazzano.eu/76561198047788587')
-const PARSER = new DOMParser()
-
 /*
- * taken from https://ionicons.com/
+ * icons taken from https://ionicons.com/
  *             Copyright (c) 2015-present Ionic (http://ionic.io/)
  */
 const ICON_UP = '<path d="M345.6 128l51.3 51.3-109.3 109.4-89.6-89.6L32 365.4 63.6 397 198 262.5l89.6 89.7 141.1-141 51.3 51.3V128H345.6z"/>'
 const ICON_DOWN = '<path d="M480 397V262.5l-51.3 51.3-141.1-141-89.6 89.7L63.6 128 32 159.6l166 166.3 89.6-89.7 109.3 109.4-51.3 51.4H480z"/>'
 
+
 function fetchData() {
     rlog('Fetching ScoreSaber-Data...')
 
-    return fetch(new Request(DATA_URL))
-        .then(response => {
-            if (!response.ok) {
-                throw `HTTP error code ${response.status}`
-            } else {
-                return response.text()
-            }
-        })
-        .then(text => PARSER.parseFromString(text, 'text/html'))
-        .then(doc => {
-            // Parse:
-            let [rankLi, ppLi, playCountLi, ] = doc.querySelectorAll(
-                '.content .columns .column:nth-child(2) > ul > li'
-            )
-
-            let [ , globalRank, globalCount, country, countryRank] = rankLi.innerHTML
-                .match(/#([0-9,]+)\s*\/\s*#([0-9,]+).*country=([a-z]+).*#([0-9,]+)/s)
-            ;
-
-            let globalPercentile = toNumber(globalRank) / toNumber(globalCount) * 100
-
-            let pp = ppLi.innerText.match(/[0-9,.]+/)[0]
-            let playCount = playCountLi.innerText.match(/[0-9]+/)[0]
-
-            let rankHistory = doc.querySelector('script:not([src])').innerText
-                .match(/datasets:.*\[{.*data:.*\[([0-9,]+)/s)[1]
-                .split(',')
-                .map(v => (+v))
-                .reverse()
-            ;
-            let globalRankChangeToday = rankHistory[1] - rankHistory[0]
-            let isGlobalRankChangeTodayUp = (globalRankChangeToday >= 0)
-            let globalRankChangeWeek = rankHistory[8] - rankHistory[0]
-            let isGlobalRankChangeWeekUp = (globalRankChangeWeek >= 0)
-
-            // Format:
-            globalRank = toLocaleNumberString(globalRank)
-            globalPercentile = globalPercentile.toFixed(2)
-            globalRankChangeToday = Math.abs(globalRankChangeToday).toLocaleString()
-            globalRankChangeWeek = Math.abs(globalRankChangeWeek).toLocaleString()
-            countryRank = toLocaleNumberString(countryRank)
-            pp = toLocaleNumberString(pp)
+    return scoresaber_getData('76561198047788587')
+        .then(data => {
+            let {
+                globalRank, globalPercentile, globalRankChangeToday, globalRankChangeWeek,
+                isGlobalRankChangeTodayUp, isGlobalRankChangeWeekUp,
+                country, countryRank, pp
+            } = data
 
             // Insert in site:
             document.getElementById('global-rank-value').innerText = 
@@ -79,10 +42,6 @@ function fetchData() {
         })
     ;
 }
-
-const toNumber = (str) => (+str.replace(',', ''));
-const toLocaleNumberString = (str) => toNumber(str).toLocaleString();
-
 
 
 // Start-up:
---
+++ src/scoresaber_lib.js
@@ -0,0 +1,84 @@
+const SCORESABER_DATA_URL = new URL('https://scoresaber-proxy.lucavazzano.eu')
+const SCORESABER_PARSER = new DOMParser()
+
+
+const _toNumber = (str) => (+str.replace(',', ''));
+const _toLocaleNumberString = (str) => _toNumber(str).toLocaleString();
+
+
+function _scoresaber_fetch(id) {
+    let targetUrl = new URL(id, SCORESABER_DATA_URL)
+    return fetch(new Request(targetUrl))
+        .then(response => {
+            if (!response.ok) {
+                throw `HTTP error code ${response.status}`
+            } else {
+                return response.text()
+            }
+        })
+    ;
+}
+
+function _scoresaber_parse(text) {
+    // Find Elements:
+    let doc = SCORESABER_PARSER.parseFromString(text, 'text/html')
+    let dataSectionSelector = '.content .columns .column:nth-child(2)'
+
+    let nameLink = doc.querySelector(
+        `${dataSectionSelector} > h5 > a`
+    )
+    let [rankLi, ppLi, playCountLi, ] = doc.querySelectorAll(
+        `${dataSectionSelector} > ul > li`
+    )
+
+    // Extract Data:
+    let name = nameLink.innerText.replace(/\W/, '');
+    
+    let [ , globalRank, globalCount, country, countryRank] = rankLi.innerHTML
+        .match(/#([0-9,]+)\s*\/\s*#([0-9,]+).*country=([a-z]+).*#([0-9,]+)/s)
+    ;
+
+    let globalPercentile = _toNumber(globalRank) / _toNumber(globalCount) * 100
+
+    let pp = ppLi.innerText.match(/[0-9,.]+/)[0]
+    let playCount = playCountLi.innerText.match(/[0-9]+/)[0]
+
+    let rankHistory = doc.querySelector('script:not([src])').innerText
+        .match(/datasets:.*\[{.*data:.*\[([0-9,]+)/s)[1]
+        .split(',')
+        .map(v => (+v))
+        .reverse()
+    ;
+    let globalRankChangeToday = rankHistory[1] - rankHistory[0]
+    let isGlobalRankChangeTodayUp = (globalRankChangeToday >= 0)
+    let globalRankChangeWeek = rankHistory[8] - rankHistory[0]
+    let isGlobalRankChangeWeekUp = (globalRankChangeWeek >= 0)
+
+    // Format:
+    globalRank = _toLocaleNumberString(globalRank)
+    globalPercentile = globalPercentile.toFixed(2)
+    globalRankChangeToday = Math.abs(globalRankChangeToday).toLocaleString()
+    globalRankChangeWeek = Math.abs(globalRankChangeWeek).toLocaleString()
+    countryRank = _toLocaleNumberString(countryRank)
+    pp = _toLocaleNumberString(pp)
+
+    return {
+        'name': name,
+        'globalRank': globalRank,
+        'globalPercentile': globalPercentile,
+        'globalRankChangeToday': globalRankChangeToday,
+        'isGlobalRankChangeTodayUp': isGlobalRankChangeTodayUp,
+        'globalRankChangeWeek': globalRankChangeWeek,
+        'isGlobalRankChangeWeekUp': isGlobalRankChangeWeekUp,
+        'country': country,
+        'countryRank': countryRank,
+        'pp': pp
+    }
+}
+
+
+function scoresaber_getData(id) {
+    return _scoresaber_fetch(id)
+        .then(text => _scoresaber_parse(text))
+    ;
+}
